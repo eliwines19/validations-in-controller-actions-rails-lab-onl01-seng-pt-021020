74ffd7dc65eee78544a85c3dda93428f32d356d4
Elijah wines <elijahwines84@yahoo.com>
2020-06-22 16:40:20 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,11 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
     redirect_to author_path(@author)
   end
 
